45a577ce5811698bfbc49ce4c7c09f0363cad9e7
David Weldon <dweldon@gmail.com>
2016-05-10 11:33:00 -0700

    Display a list of notes.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import uuid from 'node-uuid';
+
 import Note from './Note';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack',
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React',
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do Laundry',
+      },
+    ];
+
+    return (
+      <div>
+        <ul>
+          {notes.map(note => <li key={note.id}>{note.task}</li>)}
+        </ul>
+      </div>
+    );
+
   }
 }
