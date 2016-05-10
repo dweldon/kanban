1aaa490092839071f432ee2e801a5231156b8637
David Weldon <dweldon@gmail.com>
2016-05-10 11:57:35 -0700

    Allow new notes to be added.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,29 +4,43 @@ import uuid from 'node-uuid';
 import Note from './Note';
 
 export default class App extends React.Component {
-  render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack',
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React',
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do Laundry',
-      },
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack',
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React',
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry',
+        },
+      ],
+    };
+
+    this.addNote = this.addNote.bind(this);
+  }
+
+  render() {
     return (
       <div>
+        <button onClick={this.addNote}>+</button>
         <ul>
-          {notes.map(note => <li key={note.id}>{note.task}</li>)}
+          {this.state.notes.map(note => <li key={note.id}>{note.task}</li>)}
         </ul>
       </div>
     );
+  }
 
+  addNote() {
+    const note = { id: uuid.v4(), task: 'New Task' };
+    const oldNotes = this.state.notes;
+    this.setState({ notes: [...oldNotes, note] });
   }
 }
